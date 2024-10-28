7f7adc85276299b1e77b54a782428a384c096a8c
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-28 17:04:03 -0300

    codigo 3

--- codigo3/codigo3.py
+++ codigo3/codigo3.py
@@ -1,5 +1,27 @@
 import pytest
 
-@pytest.fixture
-def lista_doida():
-    return [1,2,3,4,5,6]
+# @pytest.fixture
+# def lista_doida():
+#     return [1,2,3,4,5,6]
+
+# @pytest.fixture
+# def soma_dobro():
+#     def dentro(numeros):
+#         return sum(x * 2 for x in numeros)
+#     return dentro
+    
+def acordar_cedo(horario):
+    if horario > 6: #Se acordar após as 6 faça:
+        return 'Tente novamente amanhã'
+    return 'Você é um guerreiro'
+     
+def tempo_exercicio(peso,tempo):
+    if tempo > 2: #Se o tempo de exercicio for maior que 2 faça:
+        peso -= 1
+        return peso
+    pass #Passa a função sem return
+
+def tem_exercicio(esporte):
+    if esporte == 'Descanso': #Se passar 'Descanso' retorna False
+        return False
+    return True
--- codigo3/test_codigo3.py
+++ codigo3/test_codigo3.py
@@ -1,5 +1,30 @@
 from codigo3 import *
 import pytest
 
-def test_soma(lista_doida):
-    assert sum(lista_doida) == 21
+# def test_soma(lista_doida):
+#     assert sum(lista_doida) == 21
+
+# def test_tamanho(lista_doida):
+#     assert len(lista_doida) == 6
+
+# def test_dobro(soma_dobro):
+#     numeros = [40] 
+#     assert soma_dobro(numeros) == 80
+
+def test_acordar_cedo():
+    assert acordar_cedo(5) != 1
+    assert acordar_cedo(6) == "Você é um guerreiro"
+    assert acordar_cedo(7) == "Tente novamente amanhã"
+    assert acordar_cedo(8) == "Tente novamente amanhã"
+
+def test_tempo_exercicio():
+    assert tempo_exercicio(5, 3) == 4 
+    assert tempo_exercicio(5, 2) is None 
+    assert tempo_exercicio(10, 4) == 9  
+    assert tempo_exercicio(0, 1) is None 
+
+def test_tem_exercicio():
+    assert tem_exercicio("Futebol") is True
+    assert tem_exercicio("Descanso") is False
+    assert tem_exercicio("Caminhada") is True
+    assert tem_exercicio("Nada") is True
